df652e315e896578c249b1241419d5640e0268ee
AdrianoC <adriano.cbagc@gmail.com>
2023-07-27 11:49:33 -0300

    Adicionei script.js

--- scripts.js
+++ scripts.js
@@ -8,21 +8,50 @@ const errorMessages = document.querySelectorAll(".error-message");
 
 form.addEventListener('submit',(e)=> {
     e.preventDefault()
+    cleanErrors()
     validateInputs()
-})
+});
+
+function cleanErrors() {
+    errorMessages.forEach((msg)=>{
+        msg.textContent = ''
+    })
+    nome.parentElement.classList.remove('error')
+    email.parentElement.classList.remove('error')
+    assunto.parentElement.classList.remove('error')
+    mensagem.parentElement.classList.remove('error')
+}
 
 function setError(input,errorMessage) {
     const errorMessageElement = input.nextElementSibling;
-
     errorMessageElement.textContent = errorMessage;
+    input.parentElement.classList.add("error")
 }
 
 
 function validateInputs() {
     // Trim - Tira os espaços em brancos
     const nomeValue = nome.value.trim()
+    const emailValue = email.value.trim()
+    const assuntoValue = assunto.value.trim()
+    const mensagemValue = mensagem.value.trim()
 
     if(nomeValue === '') {
-        setError(nome,"Nome não pode ficar em branco")
+        setError(nome,"Nome não pode ficar em branco");
     }
+    if(emailValue === '') {
+        setError(email,"E-mail não pode ficar em branco");
+    } else if(!isEmailValid(emailValue)) {
+        setError(email,"E-mail inválido");
+    }
+    if(assuntoValue === '') {
+        setError(assunto,"Assunto não pode ficar em branco");
+    }
+    if(mensagemValue === '') {
+        setError(mensagem,"Mensagem não pode ficar em branco");
+    }
+}
+function isEmailValid(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+
 }
